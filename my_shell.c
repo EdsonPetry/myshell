88f89b9a61d955451abd622797394c05dfc75c0d
Edson Petry <edsonlbpetry@gmail.com>
2025-11-13 19:27:13 -0500

    fix(format): applied clang formatter to my_shell.c

--- my_shell.c
+++ my_shell.c
@@ -4,11 +4,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        //no input, so enter interactive mode
-        return 0;
-    } else if (argc == 2) {
-        //input give, excutes the given file
-        return 0;
-    }
+  if (argc == 1) {
+    // no input, so enter interactive mode
+    return 0;
+  } else if (argc == 2) {
+    // input give, excutes the given file
+    return 0;
+  }
 }
