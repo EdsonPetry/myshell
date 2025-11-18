0657a54b37e9dc08789fa59124fb14599ab11b58
ben <by240@scarletmail.rutgers.edu>
2025-11-18 09:30:18 -0500

    implementing builtin functions

--- executer.c
+++ executer.c
@@ -7,30 +7,87 @@
 
 #define BUFFER_SIZE 1024 // 1kb
 
-typedef struct {
-  char **args;
-  int num_args;
-} Command;
+//finds if a file exists 
+char *findFunction(char *function) {
+  if (access("/usr/local/bin", F_OK)) {
+    return ("/usr/local/bin/%s", function);
+  } else if (access("/usr/bin", F_OK)) {
+    return ("/usr/bin/%s", function);
+  } else if (access("/bin", F_OK)) {
+    return ("/bin/%s", function);
+  } else {
+    return NULL;
+  }
+}
 
-typedef struct ParsedCmd {
-  Command *commands;
-  int num_commands;
+int cd(char *destination) { 
+  if (chdir(destination) != 0) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
 
-  char *input_file;
-  char *output_file;
+int pwd(int fd) {
+  char *buffer = malloc(BUFFER_SIZE * sizeof(char));
+  if (getcwd(buffer, BUFFER_SIZE) != 0) {
+    return EXIT_FAILURE;
+  }
+  write(fd, buffer, BUFFER_SIZE);
+  return EXIT_SUCCESS;
+}
 
-  int is_and;
-  int is_or;
-} ParsedCmd;
+int which(char *function) {
+  //If the argument to which is a builtin function, fail
+  if (strcmp(function, "cd") == 0 || strcmp(function, "pwd") == 0 || strcmp(function, "which") == 0 || strcmp(function, "exit") == 0 || strcmp(function, "die") == 0) {
+    return EXIT_FAILURE;
+  } else {
+    printf(findFunction(function));
+  }
+  return EXIT_SUCCESS;
+}
 
+/*
+If the command is not a builtin function, the function will return 0, otherwise, it will return the following:
+1 - cd
+2 - pwd
+3 - which
+4 - exit
+5 - die
+*/
+int whichFunction(char *command) {
+  if (strcmp(command, "cd") == 0) {
+    return 1;
+  } else if (strcmp(command, "pwd") == 0) {
+    return 2;
+  } else if (strcmp(command, "which") == 0) {
+    return 3;
+  } else if (strcmp(command, "exit") == 0) {
+    return 4;
+  } else if (strcmp(command, "die") == 0) {
+    return 5;
+  } else {
+    return 0;
+  }
+}
 
 /* 
 Possible return status are: 
 0: success 
 1: failure
 */
-int execute(ParsedCmd *parsed_command, int is_interactive, int fd) {
+int execute(ParsedCmd *parsed_command, int prevState, int is_interactive, int fd) {
+  if (prevState == EXIT_SUCCESS && parsed_command->is_or == 1) {
+    return prevState;
+  }
+  if (prevState == EXIT_FAILURE && parsed_command->is_and == 1) {
+    return prevState;
+  }
+  if (parsed_command == NULL) {
+    return prevState;
+  }
+
   int final_state = 0;
+  
   Command *commands_list = parsed_command->commands;
   for (int i = 0; i < parsed_command->num_commands; i++) {
       if (commands_list[i].num_args != 0) {
--- parser.h
+++ parser.h
@@ -1,7 +1,21 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct ParsedCmd ParsedCmd;
+typedef struct {
+  char **args;
+  int num_args;
+} Command;
+
+typedef struct ParsedCmd {
+  Command *commands;
+  int num_commands;
+
+  char *input_file;
+  char *output_file;
+
+  int is_and;
+  int is_or;
+} ParsedCmd;
 
 ParsedCmd *parse(const char *line);
 void free_parsed_cmd(ParsedCmd *cmd);
